da870c6ffda046946985fc427a3e1ae5d92309c4
Adrian Sharpe <asharpe@nationaljournal.com>
2020-05-23 15:09:49 -0400

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import KeepAwake from "react-native-keep-awake";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+// export default function App() {
+//     return (
+//         <View style={styles.container}>
+//             <Text>Hello World!!</Text>
+//         </View>
+//     );
+// }
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <StatusBar style={{ backgroundColor: "transparent" }} />
+                <Text style={styles.timeText}>Time Goes Here</Text>
+                <Text style={styles.dateText}>Date Goes Here</Text>
+                <KeepAwake />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    timeText: {
+        color: "#999999",
+        fontSize: 150
+    },
+    dateText: {
+        color: "#999999",
+        fontSize: 150
+    }
 });
